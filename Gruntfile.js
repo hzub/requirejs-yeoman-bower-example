d5b74c80057d30234d733fa2fbd0828fc694cd21
Hubert Zub <hubert.zub@gmail.com>
2015-01-06 00:01:24 +0100

    Removed imagemin from npm/bower dependencies, it always mess up on windows due to long directory paths.

--- Gruntfile.js
+++ Gruntfile.js
@@ -324,7 +324,6 @@ module.exports = function(grunt) {
             ],
             dist: [
                 'copy:styles',
-                'imagemin',
                 'svgmin'
             ]
         },
